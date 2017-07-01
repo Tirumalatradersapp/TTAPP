05c545daf8f541a864f53403e743acff4b4fabf8
sagarika-halya <sagarika@halyaagro.com>
2017-07-01 18:19:38 +0530

    log file

--- app/src/main/java/com/ttapp/Login.java
+++ app/src/main/java/com/ttapp/Login.java
@@ -2,6 +2,7 @@ package com.ttapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Log.i("hi", "onCreate: >>>");
     }
 }
