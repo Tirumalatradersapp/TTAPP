caa8653f900f38521dc097f3e8224e35f7d309da
sagarika-halya <sagarika@halyaagro.com>
2017-07-01 18:14:42 +0530

    added splash

---
+++ app/src/main/java/com/ttapp/Splash.java
@@ -0,0 +1,33 @@
+package com.ttapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class Splash extends AppCompatActivity {
+
+    private int SPLASH_TIME_OUT=2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+          /*to make the screen as full screen*/
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        launchScreen();
+    }
+
+    private void launchScreen() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash.this,Login.class));
+                Splash.this.finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
